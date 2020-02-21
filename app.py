a83b1a4a124196ec06862943d062d20dd66d7e2e
Mickkay22 <Mikayla.Kosmala@gmail.com>
2020-02-20 23:07:40 -0800

    reduced the amount of extra code that wasn't needed

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, Response
-#from flask_pymongo import PyMongo
-from scipy import ndimage, misc
 import numpy as np
 import os
 import base64
 from camera import VideoCamera
-
 
 
 app = Flask(__name__)
@@ -13,12 +10,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
+    #Video streaming home page.
     return render_template('index.html')
 
 
 def gen(camera):
-    """Video streaming generator function."""
+    #Video streaming generator function.
     while True:
         frame = camera.get_frame2()
         yield (b'--frame\r\n'
@@ -27,7 +24,7 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
+    #Video streaming route. Put this in the src attribute of an img tag.
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
--- camera.py
+++ camera.py
@@ -9,7 +9,6 @@ class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
 
-
     def __del__(self):
         self.video.release()
         
